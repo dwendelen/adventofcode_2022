ba95ecc4e497b0c9ac8a97b9c9e0216625a8ce38
Daan Wendelen <daanwendelen@gmail.com>
2022-12-07 13:41:11 +0100

    Day01

---
+++ src/day01/day01_b.rs
@@ -0,0 +1,36 @@
+extern crate core;
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let file = File::open("src/day01/input.txt").unwrap();
+    let mut reader = BufReader::new(file);
+    let mut lines = Vec::new();
+    loop {
+        let mut line = String::new();
+        let sz = reader.read_line(&mut line);
+        match sz {
+            Ok(szz) => {
+                if szz == 0 {
+                    break
+                } else {
+                    if line.ends_with("\n") {
+                        line = line.chars().take(line.len() - 1).collect()
+                    }
+                    lines.push(line)
+                }
+            }
+            _ => { panic!("") }
+        }
+    }
+    let mut all: Vec<i32> = lines.split(|l| l == "")
+        .map(|g| g.iter().fold(0, |a, b| a + b.parse::<i32>().unwrap()))
+        .collect();
+    all.sort();
+    all.reverse();
+    let res = all.as_slice()[..3]
+        .iter().fold(0, |a,b| a + b);
+
+    println!("{res}");
+}
